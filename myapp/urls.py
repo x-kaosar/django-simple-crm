df0c2a9e0de87156defe6d20bb273f49b2bb1577
Kawsar Ahmed <mdkaosar01k@gmail.com>
2023-08-07 16:51:00 +0600

    Added two file called user.html and decorators.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
 
 
+    path('user/', views.userPage, name='user'),
     path('', views.dashboard, name='dashboard'),
     path('product/', views.product, name='product'),
     path('customer/<int:id>/', views.customer, name="customer"),
+    
+
     path('create_order/', views.createOrder, name='create_order'),
     path('update_order/<int:id>/', views.updateOrder, name='update_order'),
     path('delete_order/<int:id>/', views.deleteOrder, name='delete_order'),
--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import OrderForm, CreateUserForm
+from .decorators import unauthenticate_user
 
 
 
@@ -93,43 +94,41 @@ def deleteOrder(request,id):
     context = {'item':order}
     return render(request, 'accounts/delete.html',context)
 
-
+@unauthenticate_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('dashboard')
-    else:
-        if request.method == "POST":
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
-            else:
-                messages.info(request, 'Username or Password is incorrect!')
-            
-        context = {}
-        return render(request, 'auth/login.html', context)
 
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.info(request, 'Username or Password is incorrect!')
+        
+    context = {}
+    return render(request, 'auth/login.html', context)
 
+@unauthenticate_user
 def signUpPage(request):
-    if request.user.is_authenticated:
-        return redirect('dashboard')
-    else:
-        form = CreateUserForm()
-        if request.method == "POST":
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'User is created for '+user)
-                return redirect('login')
-        context = {'form':form}
-        return render(request, 'auth/signup.html', context)
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'User is created for '+user)
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'auth/signup.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('login')
 
+def userPage(request):
+    context = {}
+    return render(request, 'accounts/user.html', context)
 
 
